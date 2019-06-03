d41bc964b99c28f623d52b1329819f57d84e9956
Javier Gonzalez <xaviergonz@gmail.com>
2019-06-03 20:08:43 +0200

    better way to clone objects

--- packages/mobx-state-tree-next/src/snapshot/fromSnapshot.ts
+++ packages/mobx-state-tree-next/src/snapshot/fromSnapshot.ts
@@ -1,10 +1,9 @@
-import { DeepPartial, Writable } from "ts-essentials"
-import { v4 as uuidV4 } from "uuid"
+import { DeepPartial } from "ts-essentials"
 import { runUnprotected } from "../action"
 import { createModelWithUuid, Model } from "../model/Model"
 import { getModelInfoForName } from "../model/modelInfo"
-import { Ref } from "../ref/Ref"
 import { failure, isArray, isMap, isModelSnapshot, isObject, isPlainObject, isSet } from "../utils"
+import { fixSnapshotIds } from "./fixSnapshotIds"
 import { isInternalKey, modelIdKey, typeofKey } from "./metadata"
 import { SnapshotInOf } from "./SnapshotOf"
 
@@ -16,47 +15,14 @@ export function fromSnapshot<T>(
   sn: T extends object ? DeepPartial<SnapshotInOf<T>> : T,
   options?: FromSnapshotOptions
 ): T {
-  const context: FromSnapshotContext = {
-    idMap: new Map(),
-    refs: [],
-  }
-
-  const obj = internalFromSnapshot(
-    sn,
-    {
-      generateNewIds: false,
-      ...options,
-    },
-    context
-  )
-
-  // fix reference ids if needed
   if (options && options.generateNewIds) {
-    runUnprotected(() => {
-      context.refs.forEach(ref => {
-        const oldId = ref.data.id
-        const newId = context.idMap.get(oldId)
-        if (newId) {
-          const data: Writable<typeof ref.data> = ref.data
-          data.id = newId
-        }
-      })
-    })
+    sn = fixSnapshotIds(sn)
   }
 
-  return obj
+  return internalFromSnapshot(sn)
 }
 
-interface FromSnapshotContext {
-  idMap: Map<string, string>
-  refs: Ref<any>[]
-}
-
-function internalFromSnapshot<T>(
-  sn: T extends object ? DeepPartial<SnapshotInOf<T>> : T,
-  options: FromSnapshotOptions,
-  context: FromSnapshotContext
-): T {
+function internalFromSnapshot<T>(sn: T extends object ? DeepPartial<SnapshotInOf<T>> : T): T {
   if (!isObject(sn)) {
     return sn as any
   }
@@ -70,37 +36,29 @@ function internalFromSnapshot<T>(
   }
 
   if (isArray(sn)) {
-    return fromArraySnapshot(sn, options, context) as any
+    return fromArraySnapshot(sn) as any
   }
 
   if (isModelSnapshot(sn)) {
-    return fromModelSnapshot(sn, options, context) as any
+    return fromModelSnapshot(sn) as any
   }
 
   if (isPlainObject(sn)) {
-    return fromPlainObjectSnapshot(sn, options, context) as any
+    return fromPlainObjectSnapshot(sn) as any
   }
 
   throw failure(`unsupported snapshot - ${sn}`)
 }
 
-function fromArraySnapshot(
-  sn: any[],
-  options: FromSnapshotOptions,
-  context: FromSnapshotContext
-): any[] {
-  return sn.map(v => internalFromSnapshot(v, options, context))
+function fromArraySnapshot(sn: any[]): any[] {
+  return sn.map(v => internalFromSnapshot(v))
 }
 
-function fromModelSnapshot(
-  sn: any,
-  options: FromSnapshotOptions,
-  context: FromSnapshotContext
-): Model {
+function fromModelSnapshot(sn: any): Model {
   const type = sn[typeofKey]
-  const oldId = sn[modelIdKey]
+  const id = sn[modelIdKey]
 
-  if (!oldId) {
+  if (!id) {
     throw failure(`a model a snapshot must contain an id (${modelIdKey}) key, but none was found`)
   }
 
@@ -109,14 +67,8 @@ function fromModelSnapshot(
     throw failure(`model with name "${type}" not found in the registry`)
   }
 
-  let id = oldId
-  if (options.generateNewIds) {
-    id = uuidV4()
-    context.idMap.set(oldId, id)
-  }
-
   const modelObj: Model = createModelWithUuid(modelInfo.class as any, id)
-  let processedSn = oldId === id ? sn : { ...sn, [modelIdKey]: id }
+  let processedSn = sn
   if (modelObj.fromSnapshot) {
     processedSn = modelObj.fromSnapshot(sn) as any
   }
@@ -125,26 +77,18 @@ function fromModelSnapshot(
   runUnprotected(() => {
     for (const [k, v] of Object.entries(processedSn)) {
       if (!isInternalKey(k)) {
-        data[k] = internalFromSnapshot(v, options, context)
+        data[k] = internalFromSnapshot(v)
       }
     }
   })
 
-  if (modelObj instanceof Ref) {
-    context.refs.push(modelObj)
-  }
-
   return modelObj
 }
 
-function fromPlainObjectSnapshot(
-  sn: any,
-  options: FromSnapshotOptions,
-  context: FromSnapshotContext
-): object {
+function fromPlainObjectSnapshot(sn: any): object {
   const plainObj: any = {}
   for (const [k, v] of Object.entries(sn)) {
-    plainObj[k] = internalFromSnapshot(v, options, context)
+    plainObj[k] = internalFromSnapshot(v)
   }
   return plainObj
 }
