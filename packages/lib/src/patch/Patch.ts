7ff26354be93b5b5e3796e3a7c1773f3e03c2215
Javier Gonzalez <xaviergonz@gmail.com>
2020-01-12 10:08:53 +0100

    improved Path interface

--- packages/lib/src/patch/Patch.ts
+++ packages/lib/src/patch/Patch.ts
@@ -1,7 +1,24 @@
 import { Path } from "../parent/pathTypes"
 
-export interface Patch {
-  readonly op: "replace" | "remove" | "add"
-  readonly path: Path
-  readonly value?: any
+export declare type Patch =
+  | PatchAddOperation<any>
+  | PatchRemoveOperation
+  | PatchReplaceOperation<any>
+
+export interface PatchBaseOperation {
+  path: Path
+}
+
+export interface PatchAddOperation<T> extends PatchBaseOperation {
+  op: "add"
+  value: T
+}
+
+export interface PatchRemoveOperation extends PatchBaseOperation {
+  op: "remove"
+}
+
+export interface PatchReplaceOperation<T> extends PatchBaseOperation {
+  op: "replace"
+  value: T
 }
