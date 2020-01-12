a4d1ebea1498ccd3a4b6dddee75fccb615cd4622
devanshj <jethmalani.devansh@gmail.com>
2020-01-12 15:42:18 +0530

    Fixes issue #92

--- packages/lib/src/model/modelDecorator.ts
+++ packages/lib/src/model/modelDecorator.ts
@@ -61,6 +61,7 @@ export const model = (name: string) => (clazz: ModelClass<AnyModel>) => {
     ...Object.getOwnPropertyDescriptor(newClazz, "name"),
     value: clazz.name,
   })
+  newClazz.__proto__ = clazz
   newClazz.prototype = clazz.prototype
   newClazz[modelInitializersSymbol] = (clazz as any)[modelInitializersSymbol]
   newClazz[modelPropertiesSymbol] = (clazz as any)[modelPropertiesSymbol]
---
+++ packages/lib/test/model/modelDecorator.test.ts
@@ -0,0 +1,41 @@
+import { model, Model } from "../../src"
+
+test("model decorator preserves static properties", () => {
+  @model("BarSimple")
+  class Bar extends Model({}) {
+    static foo = "foo"
+  }
+
+  expect(Bar.foo).toBe("foo")
+})
+
+test("model decorator preserves static property getters", () => {
+  @model("BarWithGetter")
+  class Bar extends Model({}) {
+    static sideEffectCount = 0
+    static get foo() {
+      return Bar.sideEffectCount++
+    }
+  }
+
+  expect(Bar.foo).toBe(0)
+  expect(Bar.foo).toBe(1)
+})
+
+test("model decorator works with static proxy gymnastics", () => {
+  class Bar extends Model({}) {}
+
+  // @ts-ignore
+  Bar = new Proxy(Bar, {
+    get: (target, key: keyof typeof Bar | "foo") => {
+      if (key === "foo") return "oof"
+      return target[key]
+    },
+  })
+
+  // @ts-ignore
+  Bar = model("BarWithProxyStuff")(Bar)
+
+  // @ts-ignore
+  expect(Bar.foo).toBe("oof")
+})
