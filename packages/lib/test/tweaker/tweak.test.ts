38c3b8f4c72aca13c820cfef3b4a08e2d0992d98
Javier Gonzalez <xaviergonz@gmail.com>
2020-07-09 23:56:53 +0200

    update deps

--- packages/lib/test/tweaker/tweak.test.ts
+++ packages/lib/test/tweaker/tweak.test.ts
@@ -1,27 +1,25 @@
 import { getGlobalConfig, runUnprotected, setGlobalConfig, toTreeNode } from "../../src"
 
-describe("tweak", () => {
-  test("array disallows undefined element when allowUndefinedArrayElements is false", () => {
-    expect(getGlobalConfig().allowUndefinedArrayElements).toBeFalsy()
+test("array disallows undefined element when allowUndefinedArrayElements is false", () => {
+  expect(getGlobalConfig().allowUndefinedArrayElements).toBeFalsy()
 
-    const array = toTreeNode<undefined[]>([])
+  const array = toTreeNode<undefined[]>([])
 
-    expect(() => {
-      runUnprotected(() => {
-        array.push(undefined)
-      })
-    }).toThrowError(/^undefined is not supported inside arrays/)
-  })
+  expect(() => {
+    runUnprotected(() => {
+      array.push(undefined)
+    })
+  }).toThrowError(/^undefined is not supported inside arrays/)
+})
 
-  test("array allows undefined element when allowUndefinedArrayElements is true", () => {
-    setGlobalConfig({ allowUndefinedArrayElements: true })
+test("array allows undefined element when allowUndefinedArrayElements is true", () => {
+  setGlobalConfig({ allowUndefinedArrayElements: true })
 
-    const array = toTreeNode<undefined[]>([])
+  const array = toTreeNode<undefined[]>([])
 
-    expect(() => {
-      runUnprotected(() => {
-        array.push(undefined)
-      })
-    }).not.toThrowError(/^undefined is not supported inside arrays/)
-  })
+  expect(() => {
+    runUnprotected(() => {
+      array.push(undefined)
+    })
+  }).not.toThrowError(/^undefined is not supported inside arrays/)
 })
