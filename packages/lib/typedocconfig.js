235ebb19ae1071ea7206ec449e41f8c0768d1aad
Javier Gonzalez <xaviergonz@gmail.com>
2020-12-16 16:04:04 +0100

    fix doc generation

--- packages/lib/typedocconfig.js
+++ packages/lib/typedocconfig.js
@@ -1,10 +1,6 @@
 module.exports = {
-  inputFiles: ["src/index.ts"],
-  module: "commonjs",
-  excludeNotExported: true,
   excludePrivate: true,
   excludeProtected: true,
-  mode: "file",
   readme: "none",
   out: "../site/public/api",
   tsconfig: "tsconfig.json",
