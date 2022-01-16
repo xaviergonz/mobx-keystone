7683c3cc284447ab2b64ab4cc0a2e883e3fe8550
Sigurd Spieckermann <2206639+sisp@users.noreply.github.com>
2022-01-16 23:20:01 +0100

    remove unused internal functions (#364)

--- packages/lib/src/modelShared/utils.ts
+++ packages/lib/src/modelShared/utils.ts
@@ -1,28 +1,9 @@
 import type { AnyDataModel } from "../dataModel/BaseDataModel"
-import { isDataModel, isDataModelClass } from "../dataModel/utils"
+import { isDataModelClass } from "../dataModel/utils"
 import type { AnyModel } from "../model/BaseModel"
-import { isModel, isModelClass } from "../model/utils"
+import { isModelClass } from "../model/utils"
 import { failure } from "../utils"
 import type { ModelClass } from "./BaseModelShared"
-
-/**
- * @ignore
- * @internal
- *
- * Asserts something is actually a class or data model.
- *
- * @param model
- * @param argName
- */
-export function assertIsClassOrDataModel(
-  model: unknown,
-  argName: string,
-  customErrMsg = "must be a class or data model instance"
-): asserts model is AnyModel | AnyDataModel {
-  if (!isModel(model) && !isDataModel(model)) {
-    throw failure(`${argName} ${customErrMsg}`)
-  }
-}
 
 /**
  * @ignore
