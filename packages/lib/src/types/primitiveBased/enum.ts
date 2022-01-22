5316759de5d44f0f7a058d7402a94719dafb6378
Sigurd Spieckermann <2206639+sisp@users.noreply.github.com>
2022-01-23 00:04:14 +0100

    improve enum runtime type-checker typing (#368)

--- packages/lib/src/types/primitiveBased/enum.ts
+++ packages/lib/src/types/primitiveBased/enum.ts
@@ -3,8 +3,13 @@ import type { IdentityType } from "../schemas"
 import { typesOr } from "../utility/or"
 import { typesLiteral } from "./primitives"
 
-function enumValues(e: any): (string | number)[] {
-  const vals = []
+interface EnumLike {
+  [k: string]: number | string
+  [v: number]: string
+}
+
+function enumValues(e: EnumLike): (string | number)[] {
+  const vals: (string | number)[] = []
   for (const k of Object.keys(e)) {
     const v = e[k]
     // we have to do this since TS does something weird
@@ -21,7 +26,7 @@ function enumValues(e: any): (string | number)[] {
  * @ignore
  * Extract enum values out of a enum object.
  */
-export type EnumValues<E> = E extends Record<
+export type EnumValues<E extends EnumLike> = E extends Record<
   infer _K, // eslint-disable-line @typescript-eslint/no-unused-vars
   infer V
 >
@@ -46,28 +51,7 @@ export type EnumValues<E> = E extends Record<
  * @param enumObject
  * @returns
  */
-export function typesEnum<E extends object>(enumObject: E): IdentityType<EnumValues<E>>
-/**
- * An enum type, based on a TypeScript alike enum object.
- * Syntactic sugar for `types.or(...enum_values.map(types.literal))`
- *
- * Example:
- * ```ts
- * enum Color {
- *   Red = "red",
- *   Green = "green"
- * }
- *
- * const colorType = types.enum(Color)
- * ```
- *
- * @template E Enum type.
- * @param enumObject
- * @returns
- */
-export function typesEnum<E = never>(enumObject: object): IdentityType<E>
-
-export function typesEnum<E = never>(enumObject: object): IdentityType<E> {
+export function typesEnum<E extends EnumLike>(enumObject: E): IdentityType<EnumValues<E>> {
   assertIsObject(enumObject, "enumObject")
 
   const literals = enumValues(enumObject).map((e) => typesLiteral(e))
