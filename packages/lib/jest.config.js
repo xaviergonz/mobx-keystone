b3b4054fbbb95f22046ce0c4ba254385b2677a01
Sigurd Spieckermann <sigurd.spieckermann@gmail.com>
2022-02-07 15:00:57 +0100

    use TS config with useDefineForClassFields=false for MobX v5

--- packages/lib/jest.config.js
+++ packages/lib/jest.config.js
@@ -1,9 +1,19 @@
+const tsconfigFiles = {
+  6: "tsconfig.json",
+  5: "tsconfig.mobx5.json",
+  // 4: "tsconfig.json", // TODO: not sure yet which config to use
+}
+
+const mobxVersion = process.env.MOBX_VERSION || "6"
+
+const tsconfigFile = tsconfigFiles[mobxVersion]
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   globals: {
     "ts-jest": {
-      tsconfig: "./test/tsconfig.json",
+      tsconfig: `./test/${tsconfigFile}`,
     },
   },
   setupFiles: ["./jest.setup.js"],
