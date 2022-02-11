b520242223311d3ebf3a19d0a7bbca1455869411
Sigurd Spieckermann <2206639+sisp@users.noreply.github.com>
2022-02-11 22:48:59 +0100

    run tests against all supported MobX versions (#389)
    * run tests against all supported MobX versions
    
    * improve CI job name
    
    * prevent cancelation of CI matrix jobs when one of them fails
    
    * use TS config with useDefineForClassFields=false for MobX v5
    
    * fix compatibilty with mobx 5
    
    * use Jest module name mapper for selecting MobX version
    
    * set MOBX_VERSION env only for test run command
    
    Co-authored-by: xaviergonz <xaviergonz@gmail.com>
    Co-authored-by: Javier Gonzalez <xaviergonz@users.noreply.github.com>

--- packages/lib/jest.config.js
+++ packages/lib/jest.config.js
@@ -1,10 +1,30 @@
+const tsconfigFiles = {
+  6: "tsconfig.json",
+  5: "tsconfig.mobx5.json",
+  4: "tsconfig.mobx4.json",
+}
+
+const mobxModuleNames = {
+  6: "mobx",
+  5: "mobx-v5",
+  4: "mobx-v4",
+}
+
+const mobxVersion = process.env.MOBX_VERSION || "6"
+
+const tsconfigFile = tsconfigFiles[mobxVersion]
+const mobxModuleName = mobxModuleNames[mobxVersion]
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   globals: {
     "ts-jest": {
-      tsconfig: "./test/tsconfig.json",
+      tsconfig: `./test/${tsconfigFile}`,
     },
   },
   setupFilesAfterEnv: ["./test/commonSetup.ts"],
+  moduleNameMapper: {
+    "^mobx$": mobxModuleName,
+  },
 }
