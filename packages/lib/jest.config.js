80b9dbf9000d74c22cd73efd895e47399126bb5e
Sigurd Spieckermann <sigurd.spieckermann@gmail.com>
2022-02-11 14:19:38 +0100

    run tests also with Babel

--- packages/lib/jest.config.js
+++ packages/lib/jest.config.js
@@ -1,9 +1,23 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  globals: {
-    "ts-jest": {
-      tsconfig: "./test/tsconfig.json",
-    },
-  },
+const config = {}
+
+switch (process.env.COMPILER || "tsc") {
+  case "tsc":
+    Object.assign(config, {
+      preset: "ts-jest",
+      testEnvironment: "node",
+      globals: {
+        "ts-jest": {
+          tsconfig: "./test/tsconfig.json",
+        },
+      },
+    })
+    break
+
+  case "babel":
+    break
+
+  default:
+    throw new Error("$COMPILER must be one of {tsc,babel}")
 }
+
+module.exports = config
