2f623ebcb95a881dc3d3e8bdb7fd5db4395ab488
xaviergonz <xaviergonz@gmail.com>
2023-12-18 18:45:43 +0100

    refactor: use ts for jest config, add test to patch

--- packages/lib/env.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mobxVersion: Number(process.env.MOBX_VERSION || "6"), // 4 | 5 | 6
-  compiler: process.env.COMPILER || "tsc", // tsc | tsc-experimental-decorators | babel | swc
-}
