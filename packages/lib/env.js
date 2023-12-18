c5a3451cf8a0691b8a2eb30b646d04f41369767b
xaviergonz <xaviergonz@gmail.com>
2023-12-18 19:15:08 +0100

    refactor: use ts for jest config

---
+++ packages/lib/env.js
@@ -0,0 +1,6 @@
+// if this file is changed then env.ts needs to be changed
+
+module.exports = {
+  mobxVersion: Number(process.env.MOBX_VERSION || "6"),
+  compiler: process.env.COMPILER || "tsc",
+}
--- packages/lib/env.ts
+++ packages/lib/env.ts
@@ -1,2 +1,4 @@
+// if this file is changed then env.js needs to be changed
+
 export const mobxVersion = Number(process.env.MOBX_VERSION || "6") as 4 | 5 | 6
 export const compiler = process.env.COMPILER || "tsc" // tsc | tsc-experimental-decorators | babel | swc
