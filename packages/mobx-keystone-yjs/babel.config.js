e879c67748567d15d3abe0d73a84213f88f322f6
xaviergonz <xaviergonz@gmail.com>
2024-01-03 23:31:23 +0100

    config fixes

---
+++ packages/mobx-keystone-yjs/babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    ["@babel/preset-typescript", { allowDeclareFields: true }],
+  ],
+  plugins: [
+    ["@babel/plugin-proposal-decorators", { version: "legacy" }],
+    ["@babel/plugin-proposal-class-properties", { loose: false }],
+  ],
+}
