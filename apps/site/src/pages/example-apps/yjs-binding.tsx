8529029c5ef2cdcb17c8624fcf76601bd087af11
xaviergonz <xaviergonz@gmail.com>
2024-07-10 18:07:05 +0200

    update deps

--- apps/site/src/pages/example-apps/yjs-binding.tsx
+++ apps/site/src/pages/example-apps/yjs-binding.tsx
@@ -1,14 +1,12 @@
+import "@iframe-resizer/child"
+
 /* eslint-disable import-x/no-unresolved */
 import BrowserOnly from "@docusaurus/BrowserOnly"
-import Head from "@docusaurus/Head"
 /* eslint-enable import-x/no-unresolved */
 import { App } from "../../../docs/examples/yjsBinding/app"
 
 const Page = () => (
   <>
-    <Head>
-      <script src="/script/iframeResizer.contentWindow.min.js"></script>
-    </Head>
     <BrowserOnly>{() => <App />}</BrowserOnly>
   </>
 )
